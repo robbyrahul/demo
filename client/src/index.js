2009f6836839155d4786b51fba3562da3367c643
robbyrahul <rahulrobby13@gmail.com>
2023-07-24 13:08:38 +0530

    navigation bar

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigationbar from './components/Navigationbar';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navigationbar/>
     <App />
   </React.StrictMode>
 );
