0f72d4a265b98f6753b9a497f1fd7f77f58d7a08
robbyrahul <rahulrobby13@gmail.com>
2023-07-25 12:20:17 +0530

    chh

--- client/src/components/demoupload.jsx
+++ client/src/components/demoupload.jsx
@@ -3,17 +3,17 @@ import { useState } from 'react';
 
 export default function Demoupload() {
   const [image, setImage] = useState({ preview: "", data: "" });
-
+  const formdata= new FormData()
   const handleFile = (e) => {
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
       data: e.target.files[0],
-    };
+    }; 
     setImage(img);
-  };
-  const handleSubmit = (e) => {
-    FormData.append('file',image.data)
-    AXIOS.post('',formdata,{'content-type':'multipart/form-data'}).then(() => {
+  }; 
+  const handleSubmit = () => {
+    formdata.append('file',image.data)
+    AXIOS.post('',formdata,{'content-type':'multipart/form-data'}).then((res) => {
         alert(res.data.msg);
     })
   }
@@ -23,7 +23,7 @@ export default function Demoupload() {
       <Container>
         <Row>
           <Col>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group>
                 <Form.Label>Upload photo</Form.Label>
                 <Form.Control type="file" name="file" required onChange={handleFile} />
